1f529b1885be0d4d31185241984d01049701e896
Aprendiz <Aprendiz@BOGDFPCGMF4135.FormacionSena.RED>
2024-02-26 14:23:39 -0500

    entidades listas para el main

--- src/main/java/org/paking/java/entities/Cupo.java
+++ src/main/java/org/paking/java/entities/Cupo.java
@@ -2,4 +2,16 @@ package org.paking.java.entities;
 
 public class Cupo {
 
+    public char nombre;
+
+    public Cupo() {
+    }
+
+    public Cupo(char nombre) {
+        this.nombre = nombre;
+    }
+
+
+    
 }
+
--- src/main/java/org/paking/java/entities/Pago.java
+++ src/main/java/org/paking/java/entities/Pago.java
@@ -1,5 +1,35 @@
 package org.paking.java.entities;
+import java.time.LocalDateTime;
 
 public class Pago {
+
+    //Atributos intrinsecos
+    public LocalDateTime fechaHoraInicio;
+    public LocalDateTime fechaHoraSalida;
+    public Double valor;
+
+    //Atributos de relacion:
+    public Vehiculo vehiculo;
+    public Cupo cupo;
+
+    //Constructor 
+    public Pago() {
+    }
+
+    //Constructor parametrizado
+    public Pago(LocalDateTime fechaHoraInicio, 
+                LocalDateTime fechaHoraSalida, 
+                Double valor, 
+                Vehiculo vehiculo,
+                Cupo cupo) {
+        this.fechaHoraInicio = fechaHoraInicio;
+        this.fechaHoraSalida = fechaHoraSalida;
+        this.valor = valor;
+        this.vehiculo = vehiculo;
+        this.cupo = cupo;
+    } 
+
+    
+    
 
 }
